a8c48bc0bfe7ed2ab29dd09eb282e02e7b948576
Vgz <vinigames1122@gmail.com>
2025-06-25 17:16:06 -0300

    Add files via upload

---
+++ CICLOAPPS/frmBuscaCEP.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace CICLOAPPS
+{
+    partial class frmBuscaCEP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblEndereco = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblComplemento = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblBairro = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblCidade = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lblUF = new System.Windows.Forms.Label();
+            this.txtCEP = new System.Windows.Forms.TextBox();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite um CEP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(11, 254);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ENDEREÇO: ";
+            // 
+            // lblEndereco
+            // 
+            this.lblEndereco.AutoSize = true;
+            this.lblEndereco.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold);
+            this.lblEndereco.Location = new System.Drawing.Point(119, 255);
+            this.lblEndereco.Name = "lblEndereco";
+            this.lblEndereco.Size = new System.Drawing.Size(0, 20);
+            this.lblEndereco.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 305);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 21);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "COMPLEMENTO: ";
+            // 
+            // lblComplemento
+            // 
+            this.lblComplemento.AutoSize = true;
+            this.lblComplemento.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblComplemento.Location = new System.Drawing.Point(157, 306);
+            this.lblComplemento.Name = "lblComplemento";
+            this.lblComplemento.Size = new System.Drawing.Size(0, 20);
+            this.lblComplemento.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 355);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 21);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "BAIRRO:";
+            // 
+            // lblBairro
+            // 
+            this.lblBairro.AutoSize = true;
+            this.lblBairro.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold);
+            this.lblBairro.Location = new System.Drawing.Point(90, 356);
+            this.lblBairro.Name = "lblBairro";
+            this.lblBairro.Size = new System.Drawing.Size(0, 20);
+            this.lblBairro.TabIndex = 7;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.label8.Location = new System.Drawing.Point(12, 411);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(77, 21);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "CIDADE: ";
+            // 
+            // lblCidade
+            // 
+            this.lblCidade.AutoSize = true;
+            this.lblCidade.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold);
+            this.lblCidade.Location = new System.Drawing.Point(95, 412);
+            this.lblCidade.Name = "lblCidade";
+            this.lblCidade.Size = new System.Drawing.Size(0, 20);
+            this.lblCidade.TabIndex = 9;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.label10.Location = new System.Drawing.Point(12, 459);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(38, 21);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "UF: ";
+            // 
+            // lblUF
+            // 
+            this.lblUF.AutoSize = true;
+            this.lblUF.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold);
+            this.lblUF.Location = new System.Drawing.Point(56, 460);
+            this.lblUF.Name = "lblUF";
+            this.lblUF.Size = new System.Drawing.Size(0, 20);
+            this.lblUF.TabIndex = 10;
+            // 
+            // txtCEP
+            // 
+            this.txtCEP.Location = new System.Drawing.Point(25, 33);
+            this.txtCEP.Name = "txtCEP";
+            this.txtCEP.Size = new System.Drawing.Size(194, 20);
+            this.txtCEP.TabIndex = 11;
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.BackColor = System.Drawing.Color.Blue;
+            this.btnPesquisar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPesquisar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.btnPesquisar.ForeColor = System.Drawing.Color.White;
+            this.btnPesquisar.Location = new System.Drawing.Point(25, 76);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(133, 42);
+            this.btnPesquisar.TabIndex = 12;
+            this.btnPesquisar.Text = "PESQUISAR\r\n";
+            this.btnPesquisar.UseVisualStyleBackColor = false;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.Red;
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSair.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.btnSair.ForeColor = System.Drawing.Color.White;
+            this.btnSair.Location = new System.Drawing.Point(207, 76);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(133, 42);
+            this.btnSair.TabIndex = 13;
+            this.btnSair.Text = "SAIR";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // frmBuscaCEP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Yellow;
+            this.ClientSize = new System.Drawing.Size(441, 604);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.txtCEP);
+            this.Controls.Add(this.lblUF);
+            this.Controls.Add(this.lblCidade);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lblBairro);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblComplemento);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblEndereco);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmBuscaCEP";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblEndereco;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblComplemento;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblBairro;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblCidade;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lblUF;
+        private System.Windows.Forms.TextBox txtCEP;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
---
+++ CICLOAPPS/frmBuscaCEP.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CICLOAPPS
+{
+    public partial class frmBuscaCEP : Form
+    {
+        public frmBuscaCEP()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://viacep.com.br/ws/" + txtCEP.Text + "/json");
+            request.AllowAutoRedirect = false;
+            HttpWebResponse ChecaServidor = (HttpWebResponse)request.GetResponse();
+            if (ChecaServidor.StatusCode != HttpStatusCode.OK)
+            {
+                MessageBox.Show("Servidor indísponivel!", "Erro HTTP", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            using (Stream webstream = ChecaServidor.GetResponseStream())
+            {
+                if (webstream != null)
+                {
+                    using (StreamReader responseReader = new StreamReader(webstream))
+                    {
+                        string response = responseReader.ReadToEnd();
+                        response = Regex.Replace(response, "[{},]", string.Empty);
+                        response = response.Replace("\"", "");
+                        string[] substrings = response.Split('\n');
+                        int cont = 0;
+                        foreach (var substring in substrings)
+                        {
+                            if (cont == 1)
+                            {
+                                string[] valor = substring.Split(":".ToCharArray());
+                                if (valor[0] == "erro")
+                                {
+                                    MessageBox.Show("CEP não encontrado!");
+                                    txtCEP.Text = string.Empty;
+                                    txtCEP.Focus();
+                                    return;
+                                }
+                            }
+                            if (cont == 2)
+                            {
+                                string[] valor = substring.Split(":".ToCharArray());
+                                lblEndereco.Text = valor[1];
+                            }
+                            if (cont == 3)
+                            {
+                                string[] valor = substring.Split(":".ToCharArray());
+                                lblComplemento.Text = valor[1];
+                            }
+                            if (cont == 5)
+                            {
+                                string[] valor = substring.Split(":".ToCharArray());
+                                lblBairro.Text = valor[1];
+                            }
+                            if (cont == 6)
+                            {
+                                string[] valor = substring.Split(":".ToCharArray());
+                                lblCidade.Text = valor[1];
+                            }
+                            if (cont == 7)
+                            {
+                                string[] valor = substring.Split(":".ToCharArray());
+                                lblUF.Text = valor[1];
+                            }
+                            cont++;
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
